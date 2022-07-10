3c09fbea62426e1f84b7a7ade9e2cdebe3a03a9b
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-07-10 17:41:31 -0300

    fix(typescript-avançado): arrumando exercicio

--- typescript-avancado/src/discriminated-union.ts
+++ typescript-avancado/src/discriminated-union.ts
@@ -2,12 +2,22 @@
 
 // Melhore esse tipo utilizando uma união discriminada de tipos mais específicos
 type GuestUser = {
+    type: 'guest';
     name: string;
 }
 
 type RegisteredUser = {
+    type: 'registered';
     name: string;
     email: string;
 }
 
 export type User = GuestUser | RegisteredUser;
+
+function afunilamento(user: User) {
+    if (user.type === 'guest') {
+        console.log(user); // Guestuser
+    } else if (user.type === 'registered') {
+        console.log(user); // Registereduser
+    }
+}
